e2be05825017dbbd4c9ad65d4f6d0b3fec24efbe
takewiki <hulilei@takewiki.com.cn>
2022-10-07 15:45:29 +0800

    v1.0

--- global.R
+++ global.R
@@ -2,6 +2,8 @@ app_id =  'cpdms'
 module_id ='mdlMngrCostCom'
 conn_cfg ='conn_cfg.R'
 
+dms_token = '9B6F803F-9D37-41A2-BDA0-70A7179AF0F3'
+
 tstk::import(app_id)
 
 
--- mdlMngrCostCom_ui.R
+++ mdlMngrCostCom_ui.R
@@ -1,20 +1,11 @@
 tabItem(tabName = module_id,
                     fluidRow(
                       column(width = 12,
-                             tabBox(title ="多变量物料管理工作台",width = 12,
+                             tabBox(title ="产品管理成本工作台",width = 12,
                                     id=paste0("tabSet_",module_id),height = '300px',
-                                     mdlMultipleMaterialUI::prdGenUI(),
-                                    mdlMultipleMaterialUI::prdSelUI(),
-                                    
-                                    mdlMultipleMaterialUI::prdCategoryUI(),
-                                    
-                                    mdlMultipleMaterialUI::prdGroupUI(),
-                                    mdlMultipleMaterialUI::propCategoryUI(),
-                                    mdlMultipleMaterialUI::propCategoryConfigUI(),
-                                    mdlMultipleMaterialUI::propValueUI()
-                                    
-                                    ,
-                                    mdlMultipleMaterialUI::propValueConfigUI()
+                                    #注册UI组件
+                                    mdlMngrCostUI::mngrCostUI()
+                                     
                                     
                                     
                                     
